2b174149579c7a35f0ad199cf455f02147a3f653
Olalekan <lekan.olapeju@yahoo.com>
2020-03-10 13:26:10 -0600

    deleting form value from textbox

--- js/task.js
+++ js/task.js
@@ -26,116 +26,74 @@ form.addEventListener('submit', function (event) {
         <li>
             <input type="checkbox">
             ` + newTask.value + `
-            <time><strong>Logged:</strong> ` 
-            + startDateString + `</time>
+            <time><strong>Logged:</strong> `
+        + startDateString + `</time>
             <button>Remove</button>
             <button>Edit</button>
             <button>Done</button>
 
         </li>
     `;
+
+    document.querySelector('[id="to-do"]').value='';
 });
 
-var taskLength = document.getElementById("new_task").childNodes.length; 
+var taskLength = document.getElementById("new_task").childNodes.length;
 console.log(taskLength);
 
-function formatDateString () {
-    // New date object instance of class: Date.
+// Function to handle date function in the scripts
+function formatDateString() {
     var date = new Date();
     var dateString =
-      date.getDate() +
-      '-' +
-      ( Number( date.getMonth() ) + 1 ) + // Month starts at zero, yuck! Lets add one!
-      '-' +
-      date.getFullYear();
+        date.getDate() +
+        '-' +
+        (Number(date.getMonth()) + 1) +
+        '-' +
+        date.getFullYear();
     return dateString;
 }
 
-// var newCheckboxes = document.querySelectorAll('ul:first-of-type li [type="checkbox"]');
+// Grab our brand new checkbox! (The last LI will always be the newest one!)
+var newCheckboxes = document.querySelectorAll('ul [type="checkbox"]');
 
-// for (var i = 0; i < newCheckboxes.length; i++) {
-//     var newCheckbox = newCheckboxes[i];
+for (var i = 0; i < newCheckboxes.length; i++) {
+    var newCheckbox = newCheckboxes[i];
+    
+    var li = newCheckbox.parentNode;
+    
+    // Listen for clicks on our delete button.
+    button.addEventListener('click', function (event) {
+        // Check if the current list item is in ACTIVE or if it is in the COMPLETED list.
+        var isInActiveList = false;
+        for (var i = 0; i < activeList.children.length; i++) {
+            if (li === activeList.children[i]) {
+                isInActiveList = true; // It is in the active list!
+            }
+        }
 
-//     //Grab the associated LI.
-//     var li = newCheckBox.parentNode;
+        // Remove the child based on list.
+        if (isInActiveList) {
+            activeList.removeChild(li);
+        } else {
+            completedList.removeChild(li);
+        }
+    });
 
-//     newCheckbox.addEventListener('click', function (event) {
-//         //Delete THIS clicked checkbox.
-//         li.removeChild(this);
+    // Listen for a click on this checkbox!
+    newCheckbox.addEventListener('click', function (event) {
+       
+        li.removeChild(newCheckbox);
 
-//         // var endTime = document.createElement('TIME');
-//         // endTime.innerHTML += '<strong>'
+        // Add in our end time string!
+        var endTime = document.createElement('TIME');
+        endTime.innerHTML += '<strong>End:</strong> ' + formatDateString();
 
-//         // Move the LI to our completed UL.
-//         completedList.appendChild(li);
-//     });
+        // Add time to the li.
+        li.appendChild(endTime);
 
+        // Move the LI to our completed UL.
+        completedList.appendChild(li);
+    });
+}
+    
 
-    //     // Grab our brand new checkbox! (The last LI will always be the newest one!)
-    //     var newCheckboxes = document.querySelectorAll( 'ul [type="checkbox"]' );
-    //     // Loop through all the checkboxes - make sure they ALL have the event each time we submit!
-    //     for ( var i = 0; i < newCheckboxes.length; i++ ) {
-    //         var newCheckbox = newCheckboxes[i];
-    //         // Grab the associated LI.
-    //         var li = newCheckbox.parentNode;
-    //         // Grab our button.
-    //         //var button = li.children[2]; // Get second child element (button!)
-
-    //         // Listen for clicks on our delete button.
-    //         button.addEventListener( 'click', function (event) {
-    //             // Check if the current list item is in ACTIVE or if it is in the COMPLETED list.
-    //             var isInActiveList = false;
-    //             for ( var i = 0; i < activeList.children.length; i++ ) {
-    //                 if ( li === activeList.children[i] ) {
-    //                     isInActiveList = true; // It is in the active list!
-    //                 }
-    //             }
-
-    //             // Remove the child based on list.
-    //             if ( isInActiveList ) {
-    //                 activeList.removeChild( li );
-    //             } else {
-    //                 completedList.removeChild( li );
-    //             }
-    //         } );
-
-    //         // Listen for a click on this checkbox!
-    //         newCheckbox.addEventListener( 'click', function ( event ) {
-    //             // Delete THIS clicked checkbox.
-    //             li.removeChild( newCheckbox );
-
-    //             // Add in our end time string!
-    //             var endTime = document.createElement( 'TIME' );
-    //             endTime.innerHTML += '<strong>End:</strong> ' + formatDateString();
-
-    //             // Add time to the li.
-    //             li.appendChild( endTime );
-
-    //             // Move the LI to our completed UL.
-    //             completedList.appendChild( li );
-    //         } );
-    //     }
-    // } );
-
-    // function formatDateString () {
-    //     // New date object instance of class: Date.
-    //     var date = new Date();
-    //     var dateString =
-    //       date.getDate() +
-    //       '-' +
-    //       ( Number( date.getMonth() ) + 1 ) + // Month starts at zero, yuck! Lets add one!
-    //       '-' +
-    //       date.getFullYear() +
-    //       ' ' +
-    //       date.getHours() +
-    //       ':' +
-    //       date.getMinutes() +
-    //       ':' +
-    //       date.getSeconds();
-    //     return dateString;
-    // }
-
-
-
-    {/* <time><strong>Start:</strong> ` + startDateString + `</time>
-<button>Delete</button> */}
